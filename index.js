c3458b5b1ff67de7dfafbaebdb434c78294f08e1
Laurin-Notemann <laurin.notemann@gmail.com>
2024-05-03 19:00:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+console.log("testtest one ");
+
+app.get("/", (req, res) => {
+  res.send("<p>test</p>")
+})
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+
+})
